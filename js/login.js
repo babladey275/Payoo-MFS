6deee61df21579c9cf0d3eeb79250248e8adc099
babladey275 <babladey275@gmail.com>
2024-09-19 14:22:07 +0600

    added home page and progressing add money section

--- js/login.js
+++ js/login.js
@@ -2,7 +2,19 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     //prevent default behavior (prevent reloading browser)
     event.preventDefault();
 
-    //get the phone number
+    //get the phone number & pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // console.log(phoneNumber, pinNumber);
+
+    //validate phone & pin
+    //this is temporary. should not do this like.
+    if(phoneNumber === '5' && pinNumber === "1234") {
+        //console.log("you are logged in");
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
 })
